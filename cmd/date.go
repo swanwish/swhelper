27dafe136588f99b6371292de4b76d4e282305aa
Stephen <liwp.stephen@gmail.com>
2018-01-10 11:23:08 +0800

    add help tools for my daily use

---
+++ cmd/date.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/urfave/cli"
+)
+
+var (
+	ShowCurrentTimeCmd = cli.Command{
+		Name:        "ct",
+		Usage:       "Show current time",
+		Description: "This command is used to show current time",
+		Action:      showCurrentTime,
+		Flags:       []cli.Flag{},
+	}
+
+	ShowCurrentWeekNumCmd = cli.Command{
+		Name:        "wn",
+		Usage:       "Show current week number",
+		Description: "This command is used to show current week number",
+		Action:      showCurrentWeekNum,
+		Flags:       []cli.Flag{},
+	}
+)
+
+func showCurrentTime(c *cli.Context) error {
+	now := time.Now()
+	currentTime := now.Format("2006-01-02 15:04:05")
+	fmt.Printf("The current time is: %s\nThe unix timestamp is: %d\n", currentTime, now.Unix())
+	return nil
+}
+
+func showCurrentWeekNum(c *cli.Context) error {
+	now := time.Now()
+	_, week := now.ISOWeek()
+	fmt.Printf("The current week number is: %d\n", week)
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+	"runtime"
+
+	"github.com/swanwish/swhelper/cmd"
+	"github.com/urfave/cli"
+)
+
+const APP_VERSION = "1.0.0"
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	app := cli.NewApp()
+	app.Name = "swhelper"
+	app.Usage = "Helper Tool for Swanwish"
+	app.Version = APP_VERSION
+	app.Commands = []cli.Command{
+		cmd.ShowCurrentTimeCmd,
+		cmd.ShowCurrentWeekNumCmd,
+	}
+	app.Flags = append(app.Flags, []cli.Flag{}...)
+	app.Run(os.Args)
+}
